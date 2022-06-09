18750380c65f01ab0431e61b672ec5d4b3d3d675
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-06-09 16:49:30 +0430

    print files

--- main.py
+++ main.py
@@ -11,50 +11,52 @@ mxf = 100000
 allEmails = []
 inputs_path = "./inputs"
 files = os.listdir(inputs_path)
-for file in files:
-    filepath = inputs_path + '/' + file
-    filename, file_extension = os.path.splitext(filepath)
-    file_extension = file_extension.lower()
-    if file_extension == ".txt":
-        try:
-            f = open(filepath, "r")
-            for email in f:
-                email = str(email).replace(' ', '').replace('\n', '')
-                isCorrect = False
-                result = ''
-                if email in allEmails:
-                    isCorrect = False
-                    result, count = saveResult(isCorrect)
-                else:
-                    allEmails.append(email)
-                    isCorrect = check(email)
-                    result, count = saveResult(isCorrect)
+print(files)
 
-                    if isCorrect:
-                        if count % 100000 == 0:
-                            # new file
-                            name = str((int(count/mxf)-1) * mxf)
-                            name += "-"
-                            name += str((int(count/mxf)) * mxf)
-                        else:
-                            # append
-                            name = str((int(count/mxf))*mxf)
-                            name += "-"
-                            name += str((int(count/mxf)+1)*mxf)
+# for file in files:
+#     filepath = inputs_path + '/' + file
+#     filename, file_extension = os.path.splitext(filepath)
+#     file_extension = file_extension.lower()
+#     if file_extension == ".txt":
+#         try:
+#             f = open(filepath, "r")
+#             for email in f:
+#                 email = str(email).replace(' ', '').replace('\n', '')
+#                 isCorrect = False
+#                 result = ''
+#                 if email in allEmails:
+#                     isCorrect = False
+#                     result, count = saveResult(isCorrect)
+#                 else:
+#                     allEmails.append(email)
+#                     isCorrect = check(email)
+#                     result, count = saveResult(isCorrect)
 
-                        saveOutputs("[" + name + "]", email)
+#                     if isCorrect:
+#                         if count % 100000 == 0:
+#                             # new file
+#                             name = str((int(count/mxf)-1) * mxf)
+#                             name += "-"
+#                             name += str((int(count/mxf)) * mxf)
+#                         else:
+#                             # append
+#                             name = str((int(count/mxf))*mxf)
+#                             name += "-"
+#                             name += str((int(count/mxf)+1)*mxf)
 
-                #
-                cond = str(isCorrect)
-                if isCorrect:
-                    cond = cond + " "
-                print("#", cond, '(' + result + ')', email)
+#                         saveOutputs("[" + name + "]", email)
 
-            f.close()
+#                 #
+#                 cond = str(isCorrect)
+#                 if isCorrect:
+#                     cond = cond + " "
+#                 print("#", cond, '(' + result + ')', email)
 
-        except Exception as e:
-            print("Error", e)
-            # return False
+#             f.close()
 
-    else:
-        print("X", "Not TXT =>", filepath)
+#         except Exception as e:
+#             print("Error", e)
+#             # return False
+
+#     else:
+#         print("X", "Not TXT =>", filepath)
