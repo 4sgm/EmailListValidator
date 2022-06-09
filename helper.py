8c4114bb021fcf581371c2d4681261db6c0d217d
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-06-09 14:10:10 +0430

    initial

---
+++ helper.py
@@ -0,0 +1,73 @@
+import re
+import smtplib
+import dns.resolver
+
+
+# Address used for SMTP MAIL FROM command
+fromAddress = 'r.ahmadifar.1377@gmail.com'
+
+# Simple Regex for syntax checking
+regex = '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$'
+
+
+def check(emailAddress):
+
+    try:
+        # Syntax check
+        match = re.match(regex, emailAddress)
+        if match == None:
+            return False
+
+        # Get domain for DNS lookup
+        splitAddress = emailAddress.split('@')
+        domain = str(splitAddress[1])
+
+        # MX record lookup
+        records = dns.resolver.resolve(domain, 'MX')
+        mxRecord = records[0].exchange
+        mxRecord = str(mxRecord)
+
+        # SMTP lib setup (use debug level for full output)
+        server = smtplib.SMTP(timeout=2)
+        server.set_debuglevel(0)
+
+        # SMTP Conversation
+        server.connect(mxRecord)
+
+        # server.local_hostname(Get local server hostname)
+        server.helo(server.local_hostname)
+        server.mail(fromAddress)
+        code, message = server.rcpt(emailAddress)
+        server.quit()
+
+        # Assume SMTP response 250 is success
+        if code == 250:
+            return True
+        else:
+            return False
+    except KeyError:
+        return False
+    except Exception as e:
+        return False
+
+
+def saveResult(correct):
+    frw = open("result.txt", "r+")
+    nums = frw.read().split("/")
+    tot = int(nums[1])
+    cur = int(nums[0])
+    tot += 1
+    if correct:
+        cur += 1
+    dw = str(cur) + "/" + str(tot)
+    frw.seek(0)
+    frw.write(dw)
+    frw.close()
+
+    return dw, cur
+
+
+def saveOutputs(fc, email):
+    frw = open("./outputs/valid " + fc + ".csv", "a")
+    frw.write(email + "\n")
+    frw.close()
---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+from helper import check, saveOutputs, saveResult
+
+#
+fres = open("result.txt", "w")
+fres.write("0/0")
+fres.close()
+
+
+mxf = 100000
+inputs_path = "./inputs"
+files = os.listdir(inputs_path)
+for file in files:
+    filepath = inputs_path + '/' + file
+    filename, file_extension = os.path.splitext(filepath)
+    file_extension = file_extension.lower()
+    if file_extension == ".txt":
+        f = open(filepath, "r")
+        for email in f:
+            email = str(email).replace(' ', '').replace('\n', '')
+            isCorrect = check(email)
+            result, count = saveResult(isCorrect)
+
+            if isCorrect:
+                if count % 100000 == 0:
+                    # new file
+                    name = str((int(count/mxf)-1) * mxf)
+                    name += "-"
+                    name += str((int(count/mxf)) * mxf)
+                else:
+                    # append
+                    name = str((int(count/mxf))*mxf)
+                    name += "-"
+                    name += str((int(count/mxf)+1)*mxf)
+
+                saveOutputs("[" + name + "]", email)
+
+            #
+            cond = str(isCorrect)
+            if isCorrect:
+                cond = cond + " "
+            print("#", cond, '('+result+')', email)
+
+        f.close()
+
+    else:
+        print("X", "Not TXT =>", filepath)
