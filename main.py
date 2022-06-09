af84b594f0d151d8f90730eabf06cb733871cacf
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-06-09 18:14:15 +0430

    show selected files

--- main.py
+++ main.py
@@ -10,55 +10,61 @@ files.sort()
 start = int(sys.argv[1])
 end = int(sys.argv[2])+1
 
-
 i = 0
+selectedFiles = []
 
 for file in files:
     i += 1
-    filepath = inputs_path + '/' + file
     if i in range(start, end):
-        filename, file_extension = os.path.splitext(filepath)
-        file_extension = file_extension.lower()
-        if file_extension == ".txt":
-            try:
-                f = open(filepath, "r")
-                for email in f:
-                    email = str(email).replace(' ', '').replace('\n', '')
+        selectedFiles.append(file)
+
+
+print(selectedFiles)
+
+for file in selectedFiles:
+    filepath = inputs_path + '/' + file
+    filename, file_extension = os.path.splitext(filepath)
+    file_extension = file_extension.lower()
+    if file_extension == ".txt":
+        try:
+            f = open(filepath, "r")
+            for email in f:
+                email = str(email).replace(' ', '').replace('\n', '')
+                isCorrect = False
+                result = ''
+                if isDuplicate(email):
                     isCorrect = False
-                    result = ''
-                    if isDuplicate(email):
-                        isCorrect = False
-                        result, count = saveResult(isCorrect)
-                    else:
-                        addEmail2List(email)
-                        isCorrect = check(email)
-                        result, count = saveResult(isCorrect)
+                    result, count = saveResult(isCorrect)
+                else:
+                    addEmail2List(email)
+                    isCorrect = check(email)
+                    result, count = saveResult(isCorrect)
 
-                        if isCorrect:
-                            if count % 100000 == 0:
-                                # new file
-                                name = str((int(count/mxf)-1) * mxf)
-                                name += "-"
-                                name += str((int(count/mxf)) * mxf)
-                            else:
-                                # append
-                                name = str((int(count/mxf))*mxf)
-                                name += "-"
-                                name += str((int(count/mxf)+1)*mxf)
-
-                            saveOutputs("[" + name + "]", email)
-
-                    #
-                    cond = str(isCorrect)
                     if isCorrect:
-                        cond = cond + " "
-                    print("#", cond, '(' + result + ')', email)
+                        if count % 100000 == 0:
+                            # new file
+                            name = str((int(count/mxf)-1) * mxf)
+                            name += "-"
+                            name += str((int(count/mxf)) * mxf)
+                        else:
+                            # append
+                            name = str((int(count/mxf))*mxf)
+                            name += "-"
+                            name += str((int(count/mxf)+1)*mxf)
 
-                f.close()
+                        saveOutputs("[" + name + "]", email)
 
-            except Exception as e:
-                print("Error", e)
-                # return False
+                #
+                cond = str(isCorrect)
+                if isCorrect:
+                    cond = cond + " "
+                print("#", cond, '(' + result + ')', email)
 
-        else:
-            print("X", "Not TXT =>", filepath)
+            f.close()
+
+        except Exception as e:
+            print("Error", e)
+            # return False
+
+    else:
+        print("X", "Not TXT =>", filepath)
