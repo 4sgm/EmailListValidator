bfe2a908c64edd7112a42a62a6c87d411c55db2b
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-06-11 00:54:04 +0430

    add download files

---
+++ files.py
@@ -0,0 +1,28 @@
+
+from flask import Flask, request, send_file
+import os
+import zipfile
+import datetime
+
+
+app = Flask(__name__)
+
+
+@app.route("/files", methods=["GET"])
+def files():
+    ts = int(datetime.datetime.now().timestamp())
+    fn = "outputs-" + str(ts) + ".zip"
+    zf = zipfile.ZipFile(fn, "w")
+    for dirname, subdirs, files in os.walk("outputs"):
+        zf.write(dirname)
+        for filename in files:
+            zf.write(os.path.join(dirname, filename))
+    zf.close()
+
+    return send_file(fn, as_attachment=True)
+
+
+if __name__ == '__main__':
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8585)
+    # send_mail(subject="test", text="Hi", files=["result.txt"])
