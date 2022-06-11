39d028683f4d42477763dbd9e5e950f4e0812742
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-06-11 13:23:28 +0430

    check duplication

--- dup.py
+++ dup.py
@@ -1,8 +1,11 @@
 
 
+from itertools import count
 import os
 
-
+allEmails = []
+mxf = 100000
+count = 0
 if __name__ == '__main__':
     files = os.listdir('outputs/')
     for file in files:
@@ -10,6 +13,25 @@ if __name__ == '__main__':
         f = open(filepath, "r")
         for email in f:
             email = str(email).replace(' ', '').replace('\n', '')
-            print("*", email, "+")
+            if email in allEmails:
+                print("Duplicated", email)
+            else:
+                allEmails.append(email)
+
+                if count % 100000 == 0:
+                    # new file
+                    name = str((int(count / mxf) - 1) * mxf)
+                    name += "-"
+                    name += str((int(count / mxf)) * mxf)
+                else:
+                    # append
+                    name = str((int(count / mxf)) * mxf)
+                    name += "-"
+                    name += str((int(count / mxf) + 1) * mxf)
+
+                frw = open("./removeDup/valid [" + name + "].csv", "a")
+                frw.write(email + "\n")
+                frw.close()
+                count += 1
 
         f.close()
