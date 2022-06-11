1f6333b75114726b878962a084166d09208ee2b4
Rasoul Ahmadifar <r.ahmadifar.1377@gmail.com>
2022-06-11 13:26:50 +0430

    check duplication

--- dup.py
+++ dup.py
@@ -14,10 +14,11 @@ if __name__ == '__main__':
         for email in f:
             email = str(email).replace(' ', '').replace('\n', '')
             if email in allEmails:
-                print("Duplicated", email)
+                print("> ", email, "Duplicated")
             else:
                 allEmails.append(email)
 
+                count += 1
                 if count % 100000 == 0:
                     # new file
                     name = str((int(count / mxf) - 1) * mxf)
@@ -32,6 +33,6 @@ if __name__ == '__main__':
                 frw = open("./removeDup/valid [" + name + "].csv", "a")
                 frw.write(email + "\n")
                 frw.close()
-                count += 1
+                print("> ", email, count, file)
 
         f.close()
